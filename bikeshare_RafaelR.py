21e7e4becd643208b41476df3f5953bc63c71fda
Rafael-Roldao <rafael.roldao@gvmail.br>
2019-12-06 19:44:23 -0300

    Refactoring - get filters to only filters

--- bikeshare_RafaelR.py
+++ bikeshare_RafaelR.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def get_filters():
+def filters():
     """
     Asks user to specify a city, month, and day to analyze.
 
@@ -195,7 +195,7 @@ def user_stats(df):
 
 def main():
     while True:
-        city, month, day = get_filters()
+        city, month, day = filters()
         df = load_data(city, month, day)
 
         time_stats(df)
